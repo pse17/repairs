dc59fecf88636aa6e36cd82174a4682f69629898
pse17 <fqibymeksm@protonmail.com>
2018-11-14 13:22:47 +0500

    Test Court model added

--- reports/models.py
+++ reports/models.py
@@ -1,10 +1,50 @@
-''' Describe models application'''
+''' Describe application models'''
 from django.db import models
 
-# Create your models here.
+class Court(models.Model):
+    '''Model representing court'''
+    id = models.CharField(max_length=8, primary_key=True, db_index=True)
+    name = models.CharField(max_length=80, null=True, blank=True)
 
-class Documents(models.Model):
-    '''Model representing technical condition reports (in form CO-7, CO-8, CO-4.1, CO-4.2)'''
+    def __str__(self):
+        """ String for representing the Court"""
+        return self.name
+
+class ServiceCentre(models.Model):
+    '''Model representing service centre'''
+    name = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        """ String for representing the service centre"""
+        return self.name
+
+class Repair(models.Model):
+    '''Model representing repair in service centre in case difficult repair'''
+
+    tiket = models.CharField(max_length=8, help_text='Ticket in service centre')
+    diagnostic_card = models.CharField(max_length=8, help_text='diagnostic card number')
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    sc = models.ForeignKey(ServiceCentre, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        """ String for representing repairs in service centre"""
+        return '%s %s' % (self.diagnostic_card, self.price)
+
+class Device(models.Model):
+    '''Model representing device which we repair'''
+    name = models.CharField(max_length=30, null=True, blank=True)
+    invent_number = models.CharField(max_length=12, null=True, blank=True)
+    serial_number = models.CharField(max_length=12, null=True, blank=True)
+
+    def __str__(self):
+        """ String for representing device"""
+        return '%s %s %s' % (self.name, self.invent_number, self.serial_number)
+
+class Tickets(models.Model):
+    '''Model representing ticket in IAC'''
+    ticket = models.CharField(max_length=6, db_index=True)
+    court = models.ForeignKey(Court, on_delete=models.SET_NULL, null=True, blank=True)
+    device = models.ForeignKey(Device, on_delete=models.SET_NULL, null=True, blank=True)
 
     NOT_DEFINED = 'n'
     IN_COURT = 'c'
@@ -30,54 +70,32 @@ class Documents(models.Model):
     co7_date = models.DateField(null=True, blank=True)
     co8_date = models.DateField(null=True, blank=True)
 
-    def __str__(self):
-        """ String for representing the Documents"""
-        return 'CO-7 %s, CO-8 %s' % (self.co7_state, self.co8_state)
-
-class Court(models.Model):
-    '''Model representing court'''
-    id = models.CharField(max_lenght=8, primary_key=True, db_index=True)
-    name = models.CharField(max_lenght=80, null=True, blank=True)
-
-    def __str__(self):
-        """ String for representing the Court"""
-        return self.name
-
-class ServiceCentre(models.Model):
-    '''Model representing service centre'''
-    name = models.CharField(max_lenght=80, null=True, blank=True) 
-
-    def __str__(self):
-        """ String for representing the service centre"""
-        return self.name
-
-class Repair(models.Model):
-    '''Model representing repair in service centre in case difficult repair'''
-
-    tiket = models.CharField(max_lenght=8, help_text='Ticket in service centre')
-    diagnostic_card = models.CharField(max_lenght=8, help_text='diagnostic card number')
-    price = models.DecimalField(max_digits=9, decimal_places=2)
-    sc = models.ForeignKey(ServiceCentre, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        """ String for representing repairs in service centre"""
-        return '%s %s' % (self.diagnostic_card, self.price)
-
-class Tickets(models.Model):
-    '''Model representing ticket in IAC'''
-    court = models.ForeignKey(Court, on_delete=models.SET_NULL, null=True, blank=True)
-
-    NOT_DEFINED = 'n'
     SERVICE_CENTRE = 'c'
     REPAIR_KIT = 'k'
     NO_PAY = 'p'
     NO_REPAIR = 'r'
     KIND_OF_REPAIR = (
         (NOT_DEFINED, 'Не определено'),
-        (SERVICE_CENTRE,'В сервисном центре'),
-        (REPAIR_KIT,'С использованием ЗИП'),
+        (SERVICE_CENTRE, 'В сервисном центре'),
+        (REPAIR_KIT, 'С использованием ЗИП'),
         (NO_PAY, 'Мелкий ремонт без затрат'),
         (NO_REPAIR, 'Ремонт не выполнен'),
     )
     kind = models.CharField(
         max_length=1, choices=KIND_OF_REPAIR, blank=True, default=NOT_DEFINED)
+
+    COURT = 'c' # In court. Not yet repair or already fixed.
+    IAC = 'i'   # In IAC. For diagnostic or already fixed.
+    SC = 's'    # In service centre.
+    DEVICE_LOCATION = (
+        (COURT, 'В суде'),
+        (IAC, 'В ИАЦ'),
+        (SC, 'В сервисном центре'),
+    )
+    location = models.CharField(
+        max_length=1, choices=DEVICE_LOCATION, blank=True, default=COURT)
+    remark = models.CharField(max_length=8, help_text='Ticket in service centre')
+
+    def __str__(self):
+        """ String for representing ticket"""
+        return self.ticket
--- reports/tests/test_models.py
+++ reports/tests/test_models.py
@@ -1,3 +1,26 @@
+'''Testing models'''
 from django.test import TestCase
 
-# Create your tests here.
+from reports.models import Court
+
+class CourtTest(TestCase):
+    ''' Test Court model'''
+
+    @classmethod
+    def setUpTestData(cls):
+        Court.objects.create(id='00AA0001', name='Первый суд')
+
+    def test_id_max_length(self):
+        court = Court.objects.get(id='00AA0001')
+        max_length = court._meta.get_field('id').max_length
+        self.assertEqual(max_length, 8)
+
+    def test_name_max_length(self):
+        court = Court.objects.get(id='00AA0001')
+        max_length = court._meta.get_field('name').max_length
+        self.assertEqual(max_length, 80)
+
+    def test_object_name(self):
+        court = Court.objects.get(id='00AA0001')
+        expected_object_name = court.name
+        self.assertEqual(expected_object_name, str(court))
