ff1eccd6c421b59a4a758f5c8c0f1daf03849e17
pse17 <fqibymeksm@protonmail.com>
2019-01-29 11:52:35 +0500

    apps lable ch

--- sticker/models.py
+++ sticker/models.py
@@ -1,11 +1,10 @@
 ''' Model for sticker app. I`m captain obvious'''
 from django.db import models
-from reports.models import Court
 
 class Sticker(models.Model):
     '''Model representing inscription on the sticker'''
 
-    court = models.ForeignKey('reports.Court', related_name='courts', \
+    court = models.ForeignKey('reps.Court', related_name='courts', \
         on_delete=models.SET_NULL, null=True)
     device_name = models.CharField(max_length=80, null=False)
     invent_number = models.CharField(max_length=18, null=False)
