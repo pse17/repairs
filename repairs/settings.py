e49ad4eb836084fe1bc1ecb0d233d12bacbde159
pse17 <fqibymeksm@protonmail.com>
2019-01-30 16:23:08 +0500

    templates_dir defined

--- repairs/settings.py
+++ repairs/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'repairs' , 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -53,10 +54,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'repairs.urls'
 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['%s\\templates\\' % (BASE_DIR)],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
